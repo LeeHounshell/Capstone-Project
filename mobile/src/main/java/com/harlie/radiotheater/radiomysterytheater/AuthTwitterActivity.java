0c278585f3ea0c3c72efbe4dfbffc32331c261bd
Lee Hounshell <lee.hounshell@gmail.com>
2016-07-04 13:51:13 -0700

    fix: WIP added installRelease gradle task + remove twitter

--- mobile/src/main/java/com/harlie/radiotheater/radiomysterytheater/AuthTwitterActivity.java
+++ mobile/src/main/java/com/harlie/radiotheater/radiomysterytheater/AuthTwitterActivity.java
@@ -2,29 +2,15 @@ package com.harlie.radiotheater.radiomysterytheater;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.TwitterAuthProvider;
 import com.harlie.radiotheater.radiomysterytheater.utils.LogHelper;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
-
-import io.fabric.sdk.android.Fabric;
 
 // FIXME
 public class AuthTwitterActivity extends BaseActivity
 {
     private final static String TAG = "LEE: <" + AuthTwitterActivity.class.getSimpleName() + ">";
-
-    private TwitterLoginButton mTwitterLoginButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,9 +44,6 @@ public class AuthTwitterActivity extends BaseActivity
         }
         LogHelper.v(TAG, "--> Firebase: user not signed in");
 
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_API_KEY, BuildConfig.TWITTER_API_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
-
         // ok, we need to authenticate
         setContentView(R.layout.activity_auth_twitter);
         configureToolbarTitleBehavior();
@@ -72,59 +55,6 @@ public class AuthTwitterActivity extends BaseActivity
                 actionBar.setDisplayShowTitleEnabled(false);
             }
         }
-
-/*
- * FIXME
- *
-        mTwitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_auth);
-        mTwitterLoginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // The TwitterSession is also available through:
-                // Twitter.getInstance().core.getSessionManager().getActiveSession()
-                TwitterSession session = result.data;
-                // TODO: Remove toast and use the TwitterSession's userID
-                // with your app's user model
-                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-            }
-            @Override
-            public void failure(TwitterException exception) {
-                LogHelper.d("TwitterKit", "Login with Twitter failure", exception);
-            }
-        });
-*/
-    }
-
-/*
- * FIXME
- */
-    private void handleTwitterSession(TwitterSession session) {
-        LogHelper.d(TAG, "handleTwitterSession:" + session);
-
-        AuthCredential credential = TwitterAuthProvider.getCredential(
-                session.getAuthToken().token,
-                session.getAuthToken().secret);
-
-        getAuth().signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        LogHelper.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
-
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        boolean success = task.isSuccessful();
-                        if (!success) {
-                            LogHelper.w(TAG, "signInWithCredential", task.getException());
-                            userLoginFailed();
-                        }
-                        else {
-                            handleAuthenticationRequestResult(success);
-                        }
-                    }
-                });
     }
 
     @Override
